1093278f18a4d3760136975c6d644a54875b7523
Art Vardanyan <akvardanyan99@gmail.com>
2025-09-12 10:47:35 -0400

    routes responding

--- server/api/server.js
+++ server/api/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const authRoute = require('./routes/auth.js');
+const usersRoute = require('./routes/users.js');
+const hotelsRoute = require('./routes/hotels.js');
+const roomsRoute = require('./routes/rooms.js');
 
 const server = express()
 server.use(express.json())
@@ -9,5 +12,12 @@ server.get('/', (req, res) => {
 });
 
 server.use('/auth', authRoute)
+server.use('/users', usersRoute)
+server.use('/hotels', hotelsRoute)
+server.use('/rooms', roomsRoute)
+
+server.use((err, req, res, next) => {
+    res.status(res.statusCode || 500).json({ message: `Server error: ${err.message}` });
+});
 
 module.exports = server
