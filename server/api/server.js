d720ee9d0172b99606f42d3f04e3d6ed29037f8d
Art Vardanyan <akvardanyan99@gmail.com>
2025-09-12 10:41:40 -0400

    auth route responding

--- server/api/server.js
+++ server/api/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const authRoute = require('./routes/auth.js');
 
 const server = express()
 server.use(express.json())
@@ -6,5 +7,7 @@ server.use(express.json())
 server.get('/', (req, res) => {
     res.send('hello from homepage');
 });
+
+server.use('/auth', authRoute)
 
 module.exports = server
