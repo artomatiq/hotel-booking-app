3af1579eb8938afeb2c4e5c022e36e02fb193d79
Art Vardanyan <akvardanyan99@gmail.com>
2025-09-12 08:32:00 -0400

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const server = require('./api/server')
+const mongoose = require('mongoose')
 
 require('dotenv').config()
 
 const PORT = process.env.PORT
+const MONGO_STRING = process.env.MONGO_STRING
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_STRING)
+        console.log('connected to db');
+    } catch (error) {
+        console.log('error connecting to db: ', error)
+        throw error
+    }
+}
 
 server.listen(PORT, () => {
+    connectDB()
     console.log(`server running on PORT ${PORT}`)
 })
