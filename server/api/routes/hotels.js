b52261fd7f16dff1a0fb96000fed2352e717b602
Art Vardanyan <akvardanyan99@gmail.com>
2025-09-19 09:31:23 -0400

    update hotel endpoint

--- server/api/routes/hotels.js
+++ server/api/routes/hotels.js
@@ -1,9 +1,33 @@
 const express = require('express')
+const HotelModel = require('../models/Hotel')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.send('hello from hotels route')
+})
+
+//create new hotel
+router.post('/', async (req, res) => {
+    const newHotel = new HotelModel(req.body)
+
+    try {
+        const savedHotel = await newHotel.save()
+        res.status(200).json(savedHotel)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//update hotel
+router.put('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const updatedHotel = await HotelModel.findByIdAndUpdate(id, {$set: req.body}, {new: true})
+        res.status(200).json(updatedHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
--- server/api/server.js
+++ server/api/server.js
@@ -11,10 +11,10 @@ server.get('/', (req, res) => {
     res.send('hello from homepage');
 });
 
-server.use('api/auth', authRoute)
-server.use('api/users', usersRoute)
-server.use('api/hotels', hotelsRoute)
-server.use('api/rooms', roomsRoute)
+server.use('/api/auth', authRoute)
+server.use('/api/users', usersRoute)
+server.use('/api/hotels', hotelsRoute)
+server.use('/api/rooms', roomsRoute)
 
 server.use((err, req, res, next) => {
     res.status(res.statusCode || 500).json({ message: `Server error: ${err.message}` });
