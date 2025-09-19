bd624008426d1c8550e0b500c09cc0fc4b766c53
Art Vardanyan <akvardanyan99@gmail.com>
2025-09-19 09:53:27 -0400

    hotel route working

--- server/api/routes/hotels.js
+++ server/api/routes/hotels.js
@@ -3,8 +3,25 @@ const HotelModel = require('../models/Hotel')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('hello from hotels route')
+//get all hotels
+router.get('/', async (req, res) => {
+    try {
+        const hotels = await HotelModel.find()
+        res.status(200).json(hotels)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//get single hotel
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const hotel = await HotelModel.findById(id)
+        res.status(200).json(hotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 //create new hotel
@@ -23,8 +40,19 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
     try {
         const id = req.params.id
-        const updatedHotel = await HotelModel.findByIdAndUpdate(id, {$set: req.body}, {new: true})
+        const updatedHotel = await HotelModel.findByIdAndUpdate(id, req.body, {new: true})
         res.status(200).json(updatedHotel)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//delete hotel
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        await HotelModel.findByIdAndDelete(id)
+        res.status(200).json({message: 'hotel successfully deleted'})
     } catch (err) {
         res.status(500).json(err)
     }
