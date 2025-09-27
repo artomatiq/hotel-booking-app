5dbb654ee6dcec50436d5e9a2676f86e8aeab99e
Art <126132705+artomatic@users.noreply.github.com>
2025-09-27 19:26:46 -0400

    register controller working (init)

--- server/api/controllers/authController.js
+++ server/api/controllers/authController.js
@@ -1,7 +1,15 @@
+const User = require('../models/User')
+
 const register = async (req, res, next) => {
     try {
-        
-    } catch (err) {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        await newUser.save()
+        res.status(201).send('User has been created')
+    } catch (err) { 
         next(err)
     }
 }
--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -3,6 +3,6 @@ const {register} = require('../controllers/authController')
 
 const router = express.Router()
 
-router.get('/register', register)
+router.post('/register', register)
 
 module.exports = router
