ef0a348fc06ff102293d74124b16b761726d8697
Art Vardanyan <akvardanyan99@gmail.com>
2025-10-10 10:18:14 -0400

    wip

--- server/api/utils/verifyToken.js
+++ server/api/utils/verifyToken.js
@@ -2,36 +2,36 @@ const jwt = require("jsonwebtoken");
 const createError = require("../utils/error");
 
 const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-  if (!token) {
-    return next(createError(401, "You are not authenticated."));
-  }
+    const token = req.cookies.access_token;
+    if (!token) {
+        return next(createError(401, "You are not authenticated."));
+    }
 
-  jwt.verify(token, process.env.JWT, (err, decoded) => {
-    if (err) return next(createError(403, "Invalid token."));
-    req.user = decoded;
-    next();
-  });
+    jwt.verify(token, process.env.JWT, (err, decoded) => {
+        if (err) return next(createError(403, "Invalid token."));
+        req.user = decoded;
+        next();
+    });
 };
 
 const verifyUser = (req, res, next) => {
-    verifyToken(req, res, () => {
+    verifyToken(req, res, next, () => {
         if (req.params.id === req.user.id || req.user.isAdmin) {
             next()
         } else {
             next(createError(403, "You are not authorized."))
         }
-    }) 
+    })
 }
 
 const verifyAdmin = (req, res, next) => {
-    verifyToken(req, res, () => {
+    verifyToken(req, res, next, () => {
         if (req.user.isAdmin) {
             next()
         } else {
             next(createError(403, "You are not authorized as an admin."))
         }
-    }) 
+    })
 }
 
 module.exports = {
