6192361139e73383f7efd5c84c24f43709da28c4
Art <126132705+artomatic@users.noreply.github.com>
2025-10-10 11:19:29 -0400

    RoomSchema created

---
+++ server/api/models/Room.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const RoomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    capacity: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    roomNumbers: [
+      {
+        number: Number,
+        reservedDates: [Date] ,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
